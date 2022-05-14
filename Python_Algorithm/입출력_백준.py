3da9841a3a6c459e0d84f062636440fee0471866
Freetargeter <supreme5876@gmail.com>
2022-05-14 16:34:43 +0900

    입출력_백준

---
+++ Python_Algorithm/입출력_백준.py
@@ -0,0 +1,12 @@
+for i in range(2):
+  print("강한친구 대한육군")
+
+print("         ,r\'\"7")
+print("r`-_   ,'  ,/")
+print(" \\. \". L_r'")
+print("   `~\/")
+print("      |")
+print("      |")
+
+a,b = input().split()
+print(a-b)
