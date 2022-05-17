6b8b2a1553309d223fb475aca32b83d3dc8511a9
Freetargeter <supreme5876@gmail.com>
2022-05-17 23:39:09 +0900

    반복문2_백준

--- Python_Algorithm/입출력_백준.py
+++ Python_Algorithm/입출력_백준.py
@@ -20,3 +20,48 @@
 # a = input()
 # print(a, "??!", sep="")
 
+# R = []
+# while True :
+#   a, b = map(int, input().split())
+#   if a==0 and b==0:
+#     break
+#   R.append(a+b)
+# for i in range(len(R)):
+#   print(R[i])
+  
+# while True:
+#     try:
+#         A, B = map(int, input().split())
+#         print(A+B)
+#     except:
+#         break
+n = 0
+c = int(input())
+a = c
+b = 20
+while True:
+  if a < 10 :
+    b = 10*a + a
+  else :
+    b = (a%10)*10 + (a%10+a//10)%10
+  a = b
+  n = n + 1
+  if c == b:
+    break
+print(n)
+
+n = int(input())
+num = n
+cnt = 0
+
+while True:
+  a = num // 10
+  b = num % 10
+  c = (a+b) % 10
+  num = (b+10)+ c
+  
+  cnt = cnt + 1
+  if(num == n):
+    break
+print(cnt)
+  
